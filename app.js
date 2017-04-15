f8b3587da2f6cbbcfbfce421b8267cf6218312a8
Amos Chen <amoschen614@gmail.com>
2017-04-15 15:52:43 -0700

    add listeners to UI elements

--- app.js
+++ app.js
@@ -4,19 +4,28 @@ class ViewManager {
 	
 	connectEventHandlers() {
 		// wire up event handler for form submit
+		document.getElementById('form-numbers').addEventListener(
+			'submit', this.onSubmit);
 	}
 
 	onSubmit(event) {
 		// block from from actually submitting
 		// (which would refresh the page)
+		event.preventDefault();
 
 		// grab the number values as strings
+		let num1 = document.getElementById('input-num1').value;
+		let num2 = document.getElementById('input-num2').value;
 
 		// cast the strings to ints
+		num1 = parseInt(num1, 10);
+		num2 = parseInt(num2, 10);
 
 		// add the numbers
+		const sum = add(num1, num2);
 
 		// output
+		alert(sum);
 	}
 }
 
